d87b34c282e86540926e1aca1a7b616ca832dd46
vinicio0097 <vinicio00097@gmail.com>
2019-05-29 05:55:30 -0600

    Cambio de directorio del main.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:notas_cliente/View/main.dart';
+import 'package:notas_cliente/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
